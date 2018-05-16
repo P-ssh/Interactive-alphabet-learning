1e3cadb2a0b1aa3d5269d7f8f85a63f3bb7a7ed0
Radim Jasinsky <radim.jasinsky@gmail.com>
2018-05-16 22:45:33 +0200

    Change Reverse proxy wrapper settings

--- project/__init__.py
+++ project/__init__.py
@@ -33,7 +33,7 @@ class ReverseProxied(object):
 
 app = Flask(__name__)
 app.config.from_object('project.config.DevelopmentConfig')
-app.wsgi_app = ReverseProxied(app.wsgi_app, script_name='/alpha')
+app.wsgi_app = ReverseProxied(app.wsgi_app, script_name='/projekty/alphabet_learning')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
